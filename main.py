5af2861b7e978759ba3bb0a03acc330f64268e63
ravi maithrey <maitrey.ind@gmail.com>
2021-08-03 11:55:32 +0530

    fixed a silly bug

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # trying out basic openCV
+import cv2
+
+# defining a video capture object
+vid = cv2.VideoCapture(0)
+
+while True:
+    # capturing video per frame
+    ret, frame = vid.read()
+    # displaying the image
+    cv2.imshow('frame', frame)
+    # setting stop button as 'q' for quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# releasing the vid capture obj
+vid.release()
+cv2.destroyAllWindows()
