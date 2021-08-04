3f25baeaacd397584cb6a23af4dcc34f3bc61f4a
ravi maithrey <maitrey.ind@gmail.com>
2021-08-04 21:13:41 +0530

    added a small, fixed bounding box

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ vid = cv2.VideoCapture(0)
 while True:
     # capturing video per frame
     ret, frame = vid.read()
+    # drawing a random bounding box
+    start_point = (5, 5)
+    end_point = (220, 220)
+
+    # Blue color in BGR
+    color = (255, 0, 0)
+
+    # Line thickness of 2 px
+    thickness = 2
+
+    # Using cv2.rectangle() method
+    # Draw a rectangle with blue line borders of thickness of 2 px
+    frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
     # displaying the image
     cv2.imshow('frame', frame)
     # setting stop button as 'q' for quit
